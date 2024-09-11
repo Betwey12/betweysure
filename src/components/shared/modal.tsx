06c70343b6d1cc3e4af254960b152c81911dac86
joshysmart <sanijoshua10@gmail.com>
2024-09-11 15:14:06 +0100

    fix

---
+++ src/components/shared/modal.tsx
@@ -0,0 +1,13 @@
+interface ModalProps {
+  children: React.ReactNode;
+}
+
+export default function Modal({ children }: ModalProps) {
+  return (
+    <div className="fixed top-0 bottom-0 left-0 right-0 flex items-center justify-center bg-black/50 px-4 z-[90]">
+      <div className="bg-white overflow-hidden rounded-lg lg:rounded-3xl flex flex-col items-center text-sm relative lg:min-w-[400px] min-w-[360px] dark:bg-blue-one">
+        {children}
+      </div>
+    </div>
+  );
+}
