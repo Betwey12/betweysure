719e37fb6beee0dbdca331ccc3f8ec671b773253
joshysmart <sanijoshua10@gmail.com>
2024-09-27 09:58:11 +0100

    fix: tour

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,8 @@
 import DashboardWrapper from "@/components/dashboard/wrapper";
+import Spinner from "@/components/ui/spinner";
 import { dashboard } from "@/constants";
 import { Metadata } from "next";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: dashboard.title,
@@ -12,5 +14,15 @@ interface DashboardLayoutProps {
 }
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
-  return <DashboardWrapper>{children}</DashboardWrapper>;
+  return (
+    <Suspense
+      fallback={
+        <div className="bg-gray-light min-h-screen flex flex-col justify-center items-center h-full dark:bg-blue-one dark:text-white">
+          <Spinner />
+        </div>
+      }
+    >
+      <DashboardWrapper>{children}</DashboardWrapper>;
+    </Suspense>
+  );
 }
--- src/components/dashboard/wrapper.tsx
+++ src/components/dashboard/wrapper.tsx
@@ -6,7 +6,7 @@ import { messaging } from "@/firebase/config";
 import { HTTPRequest } from "@/api";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "react-toastify";
-import { useParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import SideNav from "./sidenav";
 import DashboardNav from "./top-nav";
 import { useAuth } from "@/hooks/useAuth";
@@ -23,13 +23,13 @@ interface DashboardLayoutProps {
 export default function DashboardWrapper({ children }: DashboardLayoutProps) {
   const router = useRouter();
   const [canRequestPermission, setCanRequestPermission] = useState(false);
-  const searchParams = useParams<{ tour: string }>();
+  const searchParams = useSearchParams();
   const { user, isLoading } = useGetUser();
   const { user: loggedInUser, isLoading: loggedInLoading } = useAuth();
   const isVerified = loggedInUser?.emailVerified || loggedInLoading;
   const hasPhone = user?.phone || loggedInLoading;
 
-  const showTour = !!searchParams.tour || false;
+  const showTour = !!searchParams.get("tour") || false;
   const { mutateAsync, isError } = useMutation({
     mutationFn: (data: { fcmToken: string }) =>
       HTTPRequest.Post("users/edit-profile", data),
@@ -132,12 +132,14 @@ export default function DashboardWrapper({ children }: DashboardLayoutProps) {
       </div>
     );
 
+  console.log(showTour, "showTour");
+
   return (
     <div className="flex flex-col h-screen lg:flex-row w-full overflow-hidden">
       <div className="hidden lg:block w-1/5">
         <SideNav />
       </div>
-      <div className="flex flex-col lg:w-4/5 h-full">
+      <div className="x flex-col lg:w-4/5 h-full">
         <DashboardNav />
         <div className="bg-gray-light p-4 lg:p-6 lg:pb-28 pb-28 overflow-y-scroll h-full relative dark:bg-blue-one dark:text-white">
           {children}
