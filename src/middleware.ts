6d48a674d6ddc1d83e612ef7d744684111ee41fe
joshysmart <sanijoshua10@gmail.com>
2024-09-27 07:55:32 +0100

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,15 +5,18 @@ export function middleware(req: NextRequest) {
   const host = req.headers.get("host");
   const protocol = req.headers.get("x-forwarded-proto") || "http";
 
+  // Check if the protocol is http or if the host doesn't start with "www."
   if (
     (protocol === "http" || host?.slice(0, 4) !== "www.") &&
     !host?.includes("localhost")
   ) {
+    // Fix the host by adding "www." if missing
     const correctedHost = host?.slice(0, 4) === "www." ? host : `www.${host}`;
-    return NextResponse.redirect(
-      `https://${correctedHost}${req.nextUrl.pathname}`,
-      301
-    );
+
+    // Preserve the query parameters by appending req.nextUrl.search
+    const redirectUrl = `https://${correctedHost}${req.nextUrl.pathname}${req.nextUrl.search}`;
+
+    return NextResponse.redirect(redirectUrl, 301);
   }
 
   return NextResponse.next();
