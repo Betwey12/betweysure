b06f2b8cfa817d9899634b9133e232065d597b9a
joshysmart <sanijoshua10@gmail.com>
2024-09-27 07:56:06 +0100

    fix

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,15 +5,12 @@ export function middleware(req: NextRequest) {
   const host = req.headers.get("host");
   const protocol = req.headers.get("x-forwarded-proto") || "http";
 
-  // Check if the protocol is http or if the host doesn't start with "www."
   if (
     (protocol === "http" || host?.slice(0, 4) !== "www.") &&
     !host?.includes("localhost")
   ) {
-    // Fix the host by adding "www." if missing
     const correctedHost = host?.slice(0, 4) === "www." ? host : `www.${host}`;
 
-    // Preserve the query parameters by appending req.nextUrl.search
     const redirectUrl = `https://${correctedHost}${req.nextUrl.pathname}${req.nextUrl.search}`;
 
     return NextResponse.redirect(redirectUrl, 301);
