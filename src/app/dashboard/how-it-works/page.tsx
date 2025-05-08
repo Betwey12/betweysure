1ee68eaffbf06335884e2fe75aac77e9d3e6bacf
joshysmart <sanijoshua10@gmail.com>
2025-05-08 16:04:03 +0100

    fix: use embed links

--- src/app/dashboard/how-it-works/page.tsx
+++ src/app/dashboard/how-it-works/page.tsx
@@ -1,13 +1,23 @@
 export default function HowItWorksPage() {
   const youtubes = [
-    "https://www.youtube.com/watch?v=V8FFe1PfRkg",
-    "https://www.youtube.com/watch?v=G4P3hCWLcZw",
+    "https://www.youtube.com/embed/V8FFe1PfRkg?si=64Q9OPOXUB-QRiE9",
+    "https://www.youtube.com/embed/G4P3hCWLcZw?si=K0P7G6boGVrLdsHc",
   ];
 
   return (
-    <div className="grid grid-cols-2 gap-10">
+    <div className="grid lg:grid-cols-2 gap-10">
       {youtubes.map((link) => (
-        <iframe key={link} width="100%" height="345" src={link}></iframe>
+        <iframe
+          key={link}
+          width="100%"
+          height="345"
+          src={link}
+          title="YouTube video player"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        />
       ))}
     </div>
   );
