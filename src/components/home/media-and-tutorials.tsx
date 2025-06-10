85591ea267c2b67bf8c40881d8bf6fed9894ef3a
joshysmart <sanijoshua10@gmail.com>
2025-06-10 14:04:14 +0100

    fix:

--- src/components/home/media-and-tutorials.tsx
+++ src/components/home/media-and-tutorials.tsx
@@ -10,18 +10,17 @@ export default function MediaAndTutorials() {
     <div className="flex flex-col gap-8 px-4 lg:px-20 md:px-10 mb-10 lg:mb-20 dark:text-white">
       <h3 className="lg:text-2xl text-xl font-semibold">{t("TITLE")}</h3>
 
-      <div className="grid lg:grid-cols-3 gap-4 lg:gap-8 justify-between">
+      <div className="grid w-full lg:grid-cols-3 gap-4 lg:gap-8 justify-between">
         {youtubes.map((link) => (
           <iframe
             key={link}
-            width="100%"
+            width="620"
             height="345"
             src={link}
             title="YouTube video player"
-            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
             referrerPolicy="strict-origin-when-cross-origin"
-            className="rounded-xl"
+            className="w-[calc(100vw-32px)] lg:w-auto block rounded-xl"
             allowFullScreen
           />
         ))}
