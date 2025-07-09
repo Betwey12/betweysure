51c54313cfd67a6cc091f8ce592efd01a54e09ed
joshysmart <sanijoshua10@gmail.com>
2025-07-09 20:21:25 +0100

    fix: brole

--- src/hooks/useXdaysMultiple.ts
+++ src/hooks/useXdaysMultiple.ts
@@ -33,40 +33,43 @@ export default function useXdaysMultiple({
     { key: "both_teams_to_score", oddsKey: "btts_yes_odds" },
   ];
 
-  const topPredictions = predictions
-    ?.filter((prediction) =>
-      oddPredictionsKeys.some(
-        (key) =>
-          +(prediction[key.key] ?? "0") >= maxAccuracy &&
-          dayjs(prediction.date).isAfter(dayjs().add(1, "minute")),
-      ),
-    )
-    .reduce(
-      (acc, prediction) => {
-        oddPredictionsKeys.forEach(({ key, oddsKey }) => {
-          if (+(prediction[key] ?? "0") >= maxAccuracy) {
-            acc.push({
-              id: prediction.id,
-              home: prediction.homeTeam,
-              away: prediction.awayTeam,
-              prediction: key,
-              odds: +(prediction[oddsKey] ?? "0"),
+  const topPredictions = predictions?.length
+    ? predictions
+        ?.filter((prediction) =>
+          oddPredictionsKeys.some(
+            (key) =>
+              +(prediction[key.key] ?? "0") >= maxAccuracy &&
+              dayjs(prediction.date).isAfter(dayjs().add(1, "minute")),
+          ),
+        )
+        .reduce(
+          (acc, prediction) => {
+            oddPredictionsKeys.forEach(({ key, oddsKey }) => {
+              if (+(prediction[key] ?? "0") >= maxAccuracy) {
+                acc.push({
+                  id: prediction.id,
+                  home: prediction.homeTeam,
+                  away: prediction.awayTeam,
+                  prediction: key,
+                  odds: +(prediction[oddsKey] ?? "0"),
+                });
+              }
             });
-          }
-        });
-        return acc;
-      },
-      [] as Array<{
-        id: number;
-        home: string;
-        away: string;
-        prediction: keyof Prediction;
-        odds: number;
-      }>,
-    )
-    .filter((prediction) => prediction.odds <= maxOdds)
-    .sort((a, b) => b.odds - a.odds)
-    ?.slice(0, noOfGames);
+            return acc;
+          },
+          [] as Array<{
+            id: number;
+            home: string;
+            away: string;
+            prediction: keyof Prediction;
+            odds: number;
+          }>,
+        )
+        .filter((prediction) => prediction.odds <= maxOdds)
+        .sort((a, b) => b.odds - a.odds)
+        ?.slice(0, noOfGames)
+    : [];
+
   const totalOdds = topPredictions?.reduce((acc, curr) => acc * curr.odds, 1);
   const returns = investment * (totalOdds ?? 0);
 
